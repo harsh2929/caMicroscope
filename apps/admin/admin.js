61821135d118989181e2723f033a8a1b5f600281
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-22 14:11:10 +0530

    Updated admin.js
    Use const or let instead of var:
    
    Use arrow functions for readability
    
    
    Instead of concatenating strings used the + operator, you can use template literals to make the code more readable.
    
    Instead of selecting the same element multiple times, you can cache it in a variable and reuse it. This can improve performance.

--- apps/admin/admin.js
+++ apps/admin/admin.js
@@ -1,22 +1,28 @@
 const pathConfig = {
-  'collection': '../collection/collection.html',
-  'user': '../user/user.html',
-
+collection: '../collection/collection.html',
+user: '../user/user.html',
 };
 
-function calculateMainHeight() {
-  const height = $(window).height() - $('#nav-bar').height();
-  $('#main').height(height);
-}
+const $navBar = $('#nav-bar');
+const $main = $('#main');
+const $sidebarMenuLinks = $('#sidebarMenu .nav-link');
+
+const calculateMainHeight = () => {
+const height = $(window).height() - $navBar.height();
+$main.height(height);
+};
+
 calculateMainHeight();
-$(window).resize(function() {
-  calculateMainHeight();
+
+$(window).resize(() => {
+calculateMainHeight();
 });
 
-function redirectPath(link, url) {
-  //
-  // console.log(this);
-  $('#sidebarMenu .nav-link').each((idx, link)=>$(link).removeClass('active'));
-  $(link).addClass('active');
-  $('#main iframe').attr('src', url);
-}
+const redirectPath = (link, url) => {
+$sidebarMenuLinks.removeClass('active');
+$(link).addClass('active');
+$main.find('iframe').attr('src', url);
+};
+
+// Example usage:
+// redirectPath('#sidebarMenu .nav-link', pathConfig.collection);
